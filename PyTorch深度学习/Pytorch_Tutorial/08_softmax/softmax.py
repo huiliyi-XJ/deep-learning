3eff30be95ddf9c7a3dbc87e9d603a557cc03e6e
XeenJay <huiliyikj@gmail.com>
2025-07-29 15:21:14 -0700

    7.29 keep fighting

--- PyTorch深度学习/Pytorch_Tutorial/08_softmax/softmax.py
+++ PyTorch深度学习/Pytorch_Tutorial/08_softmax/softmax.py
@@ -1,57 +1,56 @@
 import torch
 from torchvision import transforms
-from torchvision import datasets
+from torchvision import datasets  # 内置数据集
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 import torch.optim as optim
 
-batch_size = 64
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    # 用均值和标准差对图像进行归一化
-    transforms.Normalize((0.1307,), (0.3081, ))
-])
+batch_size = 64  # 每次处理64张图片
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        # 用均值和标准差对图像进行归一化
+        transforms.Normalize((0.1307,), (0.3081,)),
+    ]
+)
 
-train_dataset = datasets.MNIST(root='./data', 
-                               train=True, 
-                               download=True, 
-                               transform=transform)
-train_loader = DataLoader(dataset=train_dataset, 
-                          batch_size=batch_size, 
-                          shuffle=True)
+train_dataset = datasets.MNIST(
+    root="./data", train=True, download=True, transform=transform
+)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-test_dataset = datasets.MNIST(root='./data', 
-                              train=False, 
-                              download=True, 
-                              transform=transform)
-test_loader = DataLoader(dataset=test_dataset, 
-                         batch_size=batch_size, 
-                         shuffle=False)
+test_dataset = datasets.MNIST(
+    root="./data", train=False, download=True, transform=transform
+)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.l1 = torch.nn.Linear(28*28*1, 512)
+        self.l1 = torch.nn.Linear(28 * 28 * 1, 512)
         self.l2 = torch.nn.Linear(512, 256)
         self.l3 = torch.nn.Linear(256, 128)
         self.l4 = torch.nn.Linear(128, 64)
         self.l5 = torch.nn.Linear(64, 10)
 
     def forward(self, x):
-        x = x.view(-1, 28*28*1)
+        x = x.view(-1, 28 * 28 * 1)
         x = F.relu(self.l1(x))
         x = F.relu(self.l2(x))
         x = F.relu(self.l3(x))
         x = F.relu(self.l4(x))
         x = self.l5(x)
         return x
+
+
 model = Net()
 
 # 定义交叉熵损失
 criterion = torch.nn.CrossEntropyLoss()
 # 定义优化器
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
 
 def train(epoch):
     running_loss = 0.0
@@ -66,9 +65,12 @@ def train(epoch):
         optimizer.step()
 
         running_loss += loss.item()
-        if batch_idx % 300 == 299:    # 每100个batch打印一次
-            print('[%d, %5d] loss: %.3f' % (epoch + 1, batch_idx + 1, running_loss / 300))
+        if batch_idx % 300 == 299:  # 每100个batch打印一次
+            print(
+                "[%d, %5d] loss: %.3f" % (epoch + 1, batch_idx + 1, running_loss / 300)
+            )
             running_loss = 0.0
+
 
 def test():
     correct = 0
@@ -82,10 +84,10 @@ def test():
             total += labels.size(0)
             # 如果预测值和标签值相等则为true，所以这里统计的是正确的数量
             correct += (predicted == labels).sum().item()
-    print('Accuracy on test set: %d %%' % (100 * correct / total))
+    print("Accuracy on test set: %d %%" % (100 * correct / total))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for epoch in range(10):  # loop over the dataset multiple times
         train(epoch)
         test()
